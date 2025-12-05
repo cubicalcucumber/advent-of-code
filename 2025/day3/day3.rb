64e3bea93b37a83285a12a0cde65ea4803e8c131
Johannes Stäbler <johannes.markus.staebler@gmail.com>
2025-12-05 15:28:03 +0100

    Make day 3 solution a bit more descriptive

--- 2025/day3/day3.rb
+++ 2025/day3/day3.rb
@@ -5,16 +5,24 @@ input = File.readlines("#{__dir__}/day3.txt")
 def to_digits(num) = num.to_s.chars.map(&:to_i)
 def to_num(digits) = digits.join.to_i
 
-def propagate(window, num)
+# Update the current best selection given a new input `num` by checking all
+# possible ways to apply `num` to the current window and choose the best one.
+# Example:
+#   window = [6,3,4], num = 5
+#   Candidates are the old window [6,3,4] as well as [6,3,5], [6,4,5] and [3,4,5].
+def update(window, num)
   candidates = [window] + window.combination(window.size - 1).map { |xs| xs << num }
-  biggest_num = candidates.map(&method(:to_num)).max
-  to_digits(biggest_num)
+  candidates.max_by { to_num(_1) }
 end
 
+# Find the maximum joltage a battery bank can produce.
 def joltage(bank, window_size:)
   ds = to_digits(bank)
-  window = ds[0...window_size]
-  to_num(ds[window_size..].reduce(window, &method(:propagate)))
+  # Start with the first `window_size` many digits as the initial best
+  # selection of batteries and update the window given each joltage number
+  # from the rest of the bank.
+  window, rest = ds[0...window_size], ds[window_size..]
+  to_num(rest.reduce(window, &method(:update)))
 end
 
 result = input.map(&:to_i)
